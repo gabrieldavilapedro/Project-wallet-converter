2e16aa22d9f91100376466e793acc9a2b6dd6b33
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2023-10-03 19:19:15 -0300

    primeiro rec

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,1 +1,10 @@
 // Coloque aqui suas actions
+
+import { User } from '../../types';
+
+export const SUBMIT_USER_DATA = 'SUBMIT_USER_DATA';
+
+export const submmitUserData = (user: User) => ({
+  type: SUBMIT_USER_DATA,
+  payload: user,
+});
